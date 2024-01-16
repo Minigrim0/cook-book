ec59470e04d8319b6cb8c84a0909a1091ac2090b
Minigrim0 <grimauflorent@gmail.com>
2024-01-16 20:25:30 +0100

    [Upd] added ability to notify frontend of backend operations

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri::Window;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn load_path(path: &str, window: Window) -> String {
+    let total = 100000;
+
+    for i in 0..total {
+        let window = window.clone();
+        std::thread::spawn(move || {
+            window
+                .emit(
+                    "loading://progress",
+                    serde_json::json!({
+                        "progress": i,
+                        "total": total,
+                    }),
+                )
+                .unwrap();
+        });
+        // Sleep
+        std::thread::sleep(std::time::Duration::from_millis(10));
+        println!("{}: {}", path, i);
+    }
+
+    format!("done with {}", path)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![load_path])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,21 +1,45 @@
-<script setup>
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/tauri";
-
-const greetMsg = ref("");
-const name = ref("");
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke("greet", { name: name.value });
-}
-</script>
-
 <template>
-  <form class="row" @submit.prevent="greet">
-    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-    <button type="submit">Greet</button>
+  <form class="row" @submit.prevent="load_path">
+    <button>Load recipes</button>
   </form>
 
-  <p>{{ greetMsg }}</p>
+  <p>{{ returnMessage }}</p>
 </template>
+
+
+<script>
+import { invoke } from "@tauri-apps/api/tauri";
+import { appWindow } from '@tauri-apps/api/window'
+
+import { open } from '@tauri-apps/api/dialog';
+
+export default {
+  name: "Greet",
+  data() {
+    return {
+      returnMessage: "",
+    }
+  },
+  methods: {
+    async load_path() {
+      const selected = await open({
+        directory: true,
+        multiple: false,
+      });
+      if (selected === null) {
+        // user cancelled the selection
+        console.log("No directory selected!");
+      } else {
+        // user selected a single directory
+        console.log(selected);
+        await invoke("load_path", { path: selected });
+      }
+    }
+  },
+  mounted() {
+    appWindow.listen("loading://progress", (data) => {
+      this.returnMessage = `Loading ${data.progress}/${data.total}`;
+    });
+  }
+};
+</script>
