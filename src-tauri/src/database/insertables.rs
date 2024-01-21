dddeec445dd1ae001ef32a59386732e412dd1ef2
Minigrim0 <grimauflorent@gmail.com>
2024-01-21 16:39:19 +0100

    [Add] exist trait for insertables structs

--- src-tauri/src/database/insertables.rs
+++ src-tauri/src/database/insertables.rs
@@ -1,21 +1,46 @@
 use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use diesel::dsl::count;
 
-pub trait Saveable {
-    fn save(&self, connection: &mut SqliteConnection) -> Result<i32, diesel::result::Error>;
+use crate::database::connection::establish_connection;
+
+pub trait DBWrapped {
+    fn exists(&self) -> bool;
+    fn save(&self) -> Result<i32, diesel::result::Error>;
 }
 
 #[derive(Insertable)]
 #[diesel(table_name = crate::schema::author)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct NewAuthor {
-    pub id: i32,
     pub type_: String,
     pub name: String,
     pub url: String,
 }
 
-impl Saveable for NewAuthor {
-    fn save(&self, connection: &mut SqliteConnection) -> Result<i32, diesel::result::Error> {
+impl DBWrapped for NewAuthor {
+    fn exists(&self) -> bool {
+        use crate::schema::author::dsl::*;
+
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+        match author
+            .filter(type_.eq(self.type_.clone()))
+            .filter(name.eq(self.name.clone()))
+            .filter(url.eq(self.url.clone()))
+            .select(count(name))
+            .first::<i64>(connection)
+        {
+            Ok(val) => val == 1,
+            Err(err) => {
+                println!("Error counting author: {}", err.to_string());
+                false
+            }
+        }
+    }
+
+    fn save(&self) -> Result<i32, diesel::result::Error> {
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
         diesel::insert_into(crate::schema::author::table)
             .values(self)
             .execute(connection)
@@ -37,8 +62,44 @@ impl Saveable for NewAuthor {
 #[diesel(table_name = crate::schema::category)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct NewCategory {
-    pub id: i32,
     pub name: String,
+}
+
+impl DBWrapped for NewCategory {
+    fn exists(&self) -> bool {
+        use crate::schema::category::dsl::*;
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        match category
+            .filter(name.eq(self.name.clone()))
+            .select(count(name))
+            .first::<i64>(connection)
+        {
+            Ok(val) => val == 1,
+            Err(err) => {
+                println!("Error counting category: {}", err.to_string());
+                false
+            }
+        }
+    }
+
+    fn save(&self) -> Result<i32, diesel::result::Error> {
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        diesel::insert_into(crate::schema::category::table)
+            .values(self)
+            .execute(connection)
+            .expect("Error saving new category");
+
+            let last_id: i32 = diesel::select(
+                diesel::dsl::sql::<diesel::sql_types::Integer>(
+                    "last_insert_rowid()"
+                )
+            )
+                .get_result(connection)
+                .expect("Error getting last insert rowid");
+        Ok(last_id)
+    }
 }
 
 
@@ -46,8 +107,44 @@ pub struct NewCategory {
 #[diesel(table_name = crate::schema::cuisine)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct NewCuisine {
-    pub id: i32,
     pub name: String,
+}
+
+impl DBWrapped for NewCuisine {
+    fn exists(&self) -> bool {
+        use crate::schema::cuisine::dsl::*;
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        match cuisine
+            .filter(name.eq(self.name.clone()))
+            .select(count(name))
+            .first::<i64>(connection)
+        {
+            Ok(val) => val == 1,
+            Err(err) => {
+                println!("Error counting cuisine: {}", err.to_string());
+                false
+            }
+        }
+    }
+
+    fn save(&self) -> Result<i32, diesel::result::Error> {
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        diesel::insert_into(crate::schema::cuisine::table)
+            .values(self)
+            .execute(connection)
+            .expect("Error saving new cuisine");
+
+            let last_id: i32 = diesel::select(
+                diesel::dsl::sql::<diesel::sql_types::Integer>(
+                    "last_insert_rowid()"
+                )
+            )
+                .get_result(connection)
+                .expect("Error getting last insert rowid");
+        Ok(last_id)
+    }
 }
 
 
@@ -55,8 +152,44 @@ pub struct NewCuisine {
 #[diesel(table_name = crate::schema::ingredient)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct NewIngredient {
-    pub id: i32,
     pub name: String,
+}
+
+impl DBWrapped for NewIngredient {
+    fn exists(&self) -> bool {
+        use crate::schema::ingredient::dsl::*;
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        match ingredient
+            .filter(name.eq(self.name.clone()))
+            .select(count(name))
+            .first::<i64>(connection)
+        {
+            Ok(val) => val == 1,
+            Err(err) => {
+                println!("Error counting ingredient: {}", err.to_string());
+                false
+            }
+        }
+    }
+
+    fn save(&self) -> Result<i32, diesel::result::Error> {
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        diesel::insert_into(crate::schema::ingredient::table)
+            .values(self)
+            .execute(connection)
+            .expect("Error saving new ingredient");
+
+            let last_id: i32 = diesel::select(
+                diesel::dsl::sql::<diesel::sql_types::Integer>(
+                    "last_insert_rowid()"
+                )
+            )
+                .get_result(connection)
+                .expect("Error getting last insert rowid");
+        Ok(last_id)
+    }
 }
 
 
@@ -64,9 +197,46 @@ pub struct NewIngredient {
 #[diesel(table_name = crate::schema::rating)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct NewRating {
-    pub id: i32,
     pub score: i32,
     pub amount: i32,
+}
+
+impl DBWrapped for NewRating {
+    fn exists(&self) -> bool {
+        use crate::schema::rating::dsl::*;
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        match rating
+            .filter(score.eq(self.score.clone()))
+            .filter(amount.eq(self.amount.clone()))
+            .select(count(score))
+            .first::<i64>(connection)
+        {
+            Ok(val) => val == 1,
+            Err(err) => {
+                println!("Error counting rating: {}", err.to_string());
+                false
+            }
+        }
+    }
+
+    fn save(&self) -> Result<i32, diesel::result::Error> {
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        diesel::insert_into(crate::schema::rating::table)
+            .values(self)
+            .execute(connection)
+            .expect("Error saving new rating");
+
+            let last_id: i32 = diesel::select(
+                diesel::dsl::sql::<diesel::sql_types::Integer>(
+                    "last_insert_rowid()"
+                )
+            )
+                .get_result(connection)
+                .expect("Error getting last insert rowid");
+        Ok(last_id)
+    }
 }
 
 
@@ -74,7 +244,6 @@ pub struct NewRating {
 #[diesel(table_name = crate::schema::recipe)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct NewRecipe {
-    pub id: i32,
     pub name: String,
     pub cook_time: i32,
     pub prep_time: i32,
@@ -84,16 +253,92 @@ pub struct NewRecipe {
     pub category_id: i32,
 }
 
+impl DBWrapped for NewRecipe {
+    fn exists(&self) -> bool {
+        use crate::schema::recipe::dsl::*;
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        match recipe
+            .filter(name.eq(self.name.clone()))
+            .filter(cook_time.eq(self.cook_time.clone()))
+            .filter(prep_time.eq(self.prep_time.clone()))
+            .filter(yield_.eq(self.yield_.clone()))
+            .select(count(name))
+            .first::<i64>(connection) {
+                Ok(val) => val == 1,
+                Err(err) => {
+                    println!("Error counting recipe: {}", err.to_string());
+                    false
+                }
+            }
+    }
+
+    fn save(&self) -> Result<i32, diesel::result::Error> {
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        diesel::insert_into(crate::schema::recipe::table)
+            .values(self)
+            .execute(connection)
+            .expect("Error saving new recipe");
+
+            let last_id: i32 = diesel::select(
+                diesel::dsl::sql::<diesel::sql_types::Integer>(
+                    "last_insert_rowid()"
+                )
+            )
+                .get_result(connection)
+                .expect("Error getting last insert rowid");
+        Ok(last_id)
+    }
+}
+
 
 #[derive(Insertable)]
 #[diesel(table_name = crate::schema::recipe_ingredient)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct NewRecipeIngredient {
-    pub id: i32,
     pub recipe_id: i32,
     pub ingredient_id: i32,
     pub unit_id: i32,
     pub amount: String,
+}
+
+impl DBWrapped for NewRecipeIngredient {
+    fn exists(&self) -> bool {
+        use crate::schema::recipe_ingredient::dsl::*;
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        match recipe_ingredient
+            .filter(recipe_id.eq(self.recipe_id.clone()))
+            .filter(ingredient_id.eq(self.ingredient_id.clone()))
+            .select(count(recipe_id))
+            .first::<i64>(connection)
+        {
+            Ok(val) => val == 1,
+            Err(err) => {
+                println!("Error counting recipe_ingredient: {}", err.to_string());
+                false
+            }
+        }
+    }
+
+    fn save(&self) -> Result<i32, diesel::result::Error> {
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        diesel::insert_into(crate::schema::recipe_ingredient::table)
+            .values(self)
+            .execute(connection)
+            .expect("Error saving new recipe <-> ingredient");
+
+            let last_id: i32 = diesel::select(
+                diesel::dsl::sql::<diesel::sql_types::Integer>(
+                    "last_insert_rowid()"
+                )
+            )
+                .get_result(connection)
+                .expect("Error getting last insert rowid");
+        Ok(last_id)
+    }
 }
 
 
@@ -101,10 +346,47 @@ pub struct NewRecipeIngredient {
 #[diesel(table_name = crate::schema::step)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct NewStep {
-    pub id: i32,
     pub recipe_id: i32,
     pub number: i32,
     pub description: String,
+}
+
+impl DBWrapped for NewStep {
+    fn exists(&self) -> bool {
+        use crate::schema::step::dsl::*;
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        match step
+            .filter(recipe_id.eq(self.recipe_id))
+            .filter(number.eq(self.number))
+            .select(count(number))
+            .first::<i64>(connection)
+        {
+            Ok(val) => val == 1,
+            Err(err) => {
+                println!("Error counting step: {}", err.to_string());
+                false
+            }
+        }
+    }
+
+    fn save(&self) -> Result<i32, diesel::result::Error> {
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        diesel::insert_into(crate::schema::step::table)
+            .values(self)
+            .execute(connection)
+            .expect("Error saving new step");
+
+            let last_id: i32 = diesel::select(
+                diesel::dsl::sql::<diesel::sql_types::Integer>(
+                    "last_insert_rowid()"
+                )
+            )
+                .get_result(connection)
+                .expect("Error getting last insert rowid");
+        Ok(last_id)
+    }
 }
 
 
@@ -112,6 +394,42 @@ pub struct NewStep {
 #[diesel(table_name = crate::schema::unit)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct NewUnit {
-    pub id: i32,
     pub name: String,
+}
+
+impl DBWrapped for NewUnit {
+    fn exists(&self) -> bool {
+        use crate::schema::unit::dsl::*;
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        match unit
+            .filter(name.eq(self.name.clone()))
+            .select(count(name))
+            .first::<i64>(connection)
+        {
+            Ok(val) => val == 1,
+            Err(err) => {
+                println!("Error counting unit: {}", err.to_string());
+                false
+            }
+        }
+    }
+
+    fn save(&self) -> Result<i32, diesel::result::Error> {
+        let mut connection: &mut SqliteConnection = &mut establish_connection();
+
+        diesel::insert_into(crate::schema::unit::table)
+            .values(self)
+            .execute(connection)
+            .expect("Error saving new unit");
+
+            let last_id: i32 = diesel::select(
+                diesel::dsl::sql::<diesel::sql_types::Integer>(
+                    "last_insert_rowid()"
+                )
+            )
+                .get_result(connection)
+                .expect("Error getting last insert rowid");
+        Ok(last_id)
+    }
 }
