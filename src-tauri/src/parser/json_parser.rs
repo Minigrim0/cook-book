f61c057e5950a59a2dd667a2e078dbbc299e80a4
Minigrim0 <grimauflorent@gmail.com>
2024-01-22 17:45:59 +0100

    [Add] rating parser

--- src-tauri/src/parser/json_parser.rs
+++ src-tauri/src/parser/json_parser.rs
@@ -1,17 +1,14 @@
 //! This module contains the functions for parsing JSON files into the database.
 use std::fs::DirEntry;
 
-use crate::database::insertables::{NewRecipe, NewAuthor, DBWrapped};
+use crate::database::insertables::{NewRecipe, NewAuthor, DBWrapped, NewRating};
 
 // Parses the author information and returns the ID of the author in the database
 fn parse_author(recipe: &serde_json::Value) -> Option<i32> {
+    // Todo: Add support for multiple authors
     if let Some(authors) = recipe.get("author") {
-        let author = authors[0].clone();
-        let recipe_author = NewAuthor {
-            type_: author["@type"].as_str().unwrap_or("unknown").to_string(),
-            name: author["name"].as_str().unwrap_or("unknown").to_string(),
-            url: author["url"].as_str().unwrap_or("unknown").to_string()
-        };
+        let author = &authors[0];
+        let recipe_author = NewAuthor::new(author);
         if let Some(id_) = recipe_author.exists() {
             println!("Author {} exists", author["name"]);
             Some(id_)
@@ -32,6 +29,35 @@ fn parse_author(recipe: &serde_json::Value) -> Option<i32> {
     }
 }
 
+fn parse_rating(recipe: &serde_json::Value) -> Option<i32> {
+    if let Some(rating) = recipe.get("aggregateRating") {
+        let rating = NewRating::new(rating);
+        println!("Working on rating {:?}", rating);
+        if let Some(id_) = rating.exists() {
+            println!("Rating exists");
+            Some(id_)
+        } else {
+            match rating.save() {
+                Ok(id) => {
+                    println!("New rating saved with id {}", id);
+                    Some(id)
+                },
+                Err(e) => {
+                    println!("Error while creating rating: {}", e.to_string());
+                    None
+                }
+            }
+        }
+    } else {
+        println!("No aggregateRating found");
+        None
+    }
+}
+
+fn parse_category(recipe: &serde_json::Value) -> Option<i32> {
+    None
+}
+
 pub fn parse_recipe(path: &DirEntry) -> Result<String, String> {
     // Open the file recipe.json and load it
     let file_path = path.path().join(r"recipe.json");
@@ -47,8 +73,20 @@ pub fn parse_recipe(path: &DirEntry) -> Result<String, String> {
         }
     };
 
-    let mut author_id: Option<i32> = parse_author(&recipe);
+    let author_id: Option<i32> = parse_author(&recipe);
+    let rating_id: Option<i32> = parse_rating(&recipe);
+    let cat_id: Option<i32> = parse_category(&recipe);
 
+    let mut recipe = NewRecipe::new(&recipe);
+    if let Some(id) = author_id {
+        recipe.author_id = id;
+    }
+    if let Some(id) = rating_id {
+        recipe.rating_id = id;
+    }
+    if let Some(id) = cat_id {
+        recipe.category_id = id;
+    }
 
     Ok(file_path.to_str().unwrap().to_string())
 }
