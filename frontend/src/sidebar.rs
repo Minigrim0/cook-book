29b670f6b9a311f2261ffe04934805e6085840b8
Minigrim0 <grimauflorent@gmail.com>
2024-09-01 15:23:38 +0200

    Semi working accordion wtf

--- frontend/src/sidebar.rs
+++ frontend/src/sidebar.rs
@@ -11,10 +11,56 @@ pub fn SideBarComponent() -> Html {
                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class={"offcanvas-body"}>
-                <h2>{"Recipes"}</h2>
-                <RecipesTreeComponent />
-                <h3>{"Ingredients"}</h3>
-                <h3>{"Categories"}</h3>
+                <div class="accordion" id="SideBarMenuAccordion">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="AccordionRecipeHeading">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#AccordionRecipe" aria-expanded="true" aria-controls="AccordionRecipe">
+                                {"Recipes"}
+                            </button>
+                        </h2>
+                        <div id="AccordionRecipe" class="accordion-collapse collapse show" aria-labelledby="AccordionRecipeHeading" data-bs-parent="#SideBarMenuAccorion">
+                            <div class="accordion-body">
+                                <RecipesTreeComponent />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="AccordionIngredientsHeading">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#AccordionIngredients" aria-expanded="false" aria-controls="AccordionIngredients">
+                                {"Ingredients"}
+                            </button>
+                        </h2>
+                        <div id="AccordionIngredients" class="accordion-collapse collapse" aria-labelledby="AccordionIngredientsHeading" data-bs-parent="#SideBarMenuAccorion">
+                            <div class="accordion-body">
+                                <strong>{"WIP"}</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="AccordionCategoriesHeading">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#AccordionCategories" aria-expanded="false" aria-controls="AccordionCategories">
+                                {"Categories"}
+                            </button>
+                        </h2>
+                        <div id="AccordionCategories" class="accordion-collapse collapse" aria-labelledby="AccordionCategoriesHeading" data-bs-parent="#SideBarMenuAccorion">
+                            <div class="accordion-body">
+                                <strong>{"WIP"}</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="AccordionToolsHeading">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#AccordionTools" aria-expanded="false" aria-controls="AccordionTools">
+                                {"Tools"}
+                            </button>
+                        </h2>
+                        <div id="AccordionTools" class="accordion-collapse collapse" aria-labelledby="AccordionToolsHeading" data-bs-parent="#SideBarMenuAccorion">
+                            <div class="accordion-body">
+                                <strong>{"WIP"}</strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     }
